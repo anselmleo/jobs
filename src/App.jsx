045b619419150738080fb9a7e33498bd9147e524
Anselm <anselmleo@gmail.com>
2024-07-18 08:26:59 +0100

    remove unused variables

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import EditJobPage from './pages/EditJobPage';
 const App = () => {
   // Add New Job
   const addJob = async (newJob) => {
-    const res = await fetch('/api/jobs', {
+    await fetch('/api/jobs', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -27,7 +27,7 @@ const App = () => {
 
   // Delete Job
   const deleteJob = async (id) => {
-    const res = await fetch(`/api/jobs/${id}`, {
+    await fetch(`/api/jobs/${id}`, {
       method: 'DELETE',
     });
     return;
@@ -35,7 +35,7 @@ const App = () => {
 
   // Update Job
   const updateJob = async (job) => {
-    const res = await fetch(`/api/jobs/${job.id}`, {
+    await fetch(`/api/jobs/${job.id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
